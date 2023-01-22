fd2b08dad1b8e5d32bfa4399018261b7ac883e17
Koc-Ali <1koc.ali@gmail.com>
2023-01-22 07:55:56 +0100

    approach with array - works better ;)

--- task_9/2022_advent_coding_9a.py
+++ task_9/2022_advent_coding_9a.py
@@ -13,88 +13,123 @@ file = open(os.path.expanduser(path_to_file))
 
 # problem solution
 
-class RopeMove:
-    Head = None
-    Tail = None
-    Start = None
+position = {
+    'occupancy': 'free',
+    'tail_mark': False
+}
 
-    def __init__(self, Right=None, Left=None, Up=None, Down=None):
-        self.Tail_Marked = False
-        self.Right = None
-        self.Left = None
-        self.Up = None
-        self.Down = None
+new_position = {
+    'occupancy': 'free',
+    'tail_mark': False
+}
 
-        if Right is not None:
-            self.Right = Right
-        if Left is not None:
-            self.Left = Left
-        if Up is not None:
-            self.Up = Up
-        if Down is not None:
-            self.Down = Down
-
-    def move_tail_right_single(self):
+line_pos = [position]
+class MoveMatrix():
+    head_pos_x = head_pos_y = 0
+    tail_pos_x = tail_pos_y = 0
+    def __init__(self):
+        self.matrix = [line_pos]
         print(self)
 
-        if (self.Head == self.Tail.Right):
-            self.Tail_Marked = True
-            return
+    def move_head_one_step_right(self):
+        max_column = len(self.matrix[0])
+        if self.head_pos_x + 1 == max_column:
+            # extend movement matrix, i.e. include one column
+            for line in self.matrix:
+                line.append(new_position)
+        # move head one pos to right
+        self.head_pos_x += 1
+        # move tail accordingly
+        self.move_tail()
 
-        if (self.Tail.Right is not None) and (self.Head == self.Tail.Right.Right):
-            self.Tail = self.Tail.Right
-        elif (self.Tail.Up.Right is not None) and (self.Head == self.Tail.Up.Right.Right):
-            self.Tail = self.Tail.Up.Right
-        elif (self.Tail.Right.Up is not None) and (self.Head == self.Tail.Right.Up.Up):
-            self.Tail = self.Tail.Right.Up
-        elif (self.Tail.Down is not None) and (self.Head == cls.Tail.Down.Down):
-            self.Tail = self.Tail.Down
-        self.Tail.Tail_Marked = True
-
-    def move_head_right_single(self):
-        # if right element does not exists create RopMove object
-        if self.Right is None:
-            self.Right = RopeMove()
-        # move head & tail
-        self.Head = self.Right
-        self.move_tail_right_single()
-
-    def move_head_right_multiple(self, steps):
+    def move_head_multiple_steps_right(self, steps):
         for i in range(steps):
-            self.move_head_right_single()
+            self.move_head_one_step_right()
 
-    def print_movement_matrix(self):
-        print('self: ', 'ojb id', self, 'Head =', self.Head, 'Tail =', self.Tail, 'Start =', self.Start, 'tail mark = ', self.Tail_Marked)
-        # print all right elements
+    def move_head_one_step_left(self):
+        if self.head_pos_x == 0:
+            # extend movement matrix, i.e. include one column from left
+            for line in self.matrix:
+                line.insert(0, new_position)
+        else:
+            # move head one pos to right
+            self.head_pos_x -= 1
+        # move tail accordingly
+        self.move_tail()
 
-        print('--- right elements ---')
-        if (self.Right is not None):
-            self.Right.print_movement_matrix()
-        # print all left elements
-        print('--- left elements ---')
-        if (self.Left is not None):
-            self.Left.print_movement_matrix()
-        # print all upper elements
-        print('--- up elements ---')
-        if (self.Up is not None):
-            self.Up.print_movement_matrix()
-        # print all down elements
-        print('--- down elements ---')
-        if (self.Down is not None):
-            self.Down.print_movement_matrix()
+    def move_head_multiple_steps_left(self, steps):
+        for i in range(steps):
+            self.move_head_one_step_left()
+    def move_head_multiple_steps_up(self, steps):
+        for i in range(steps):
+            self.move_head_one_step_up()
+
+    def move_head_one_step_up(self):
+        if self.head_pos_y == 0:
+            # extend movement matrix, i.e. include one row
+            new_line = [new_position]
+            for i in range(len(self.matrix[0])-1):
+                new_line.append(new_position)
+            self.matrix.insert(self.head_pos_y, new_line)
+        # move head one pos to right
+        else:
+            self.head_pos_y -= 1
+        # move tail accordingly
+        self.move_tail()
+
+    def move_head_multiple_steps_down(self, steps):
+        for i in range(steps):
+            self.move_head_one_step_down()
+
+
+    def move_head_one_step_down(self):
+        max_row = len(self.matrix)
+        if self.head_pos_y + 1 == max_row:
+            # extend movement matrix, i.e. include one row
+            new_line = [new_position]
+            for i in range(len(self.matrix[0])-1):
+                new_line.append(new_position)
+            self.matrix.append(new_line)
+        # move head one pos to down
+        self.head_pos_y += 1
+        # move tail accordingly
+        self.move_tail()
+
+    def move_tail(self):
+        print('empty move tail')
+
+    def print_move_matrix(self):
+        for line in self.matrix:
+            str_line = ''
+            for pos in line:
+                str_line += 'M' if pos['tail_mark'] == True else '.'
+            print(str_line)
+
+myMoveMatrix = MoveMatrix()
+# Set StartingPosition
+myMoveMatrix.matrix[0][0]['tail_mark'] = True
+
+myMoveMatrix.move_head_multiple_steps_right(2)
+print(myMoveMatrix)
+
+
+myMoveMatrix.move_head_multiple_steps_left(5)
+print(myMoveMatrix)
+
+myMoveMatrix.move_head_multiple_steps_up(3)
+print(myMoveMatrix)
+
+myMoveMatrix.move_head_multiple_steps_down(4)
+print(myMoveMatrix)
+
+myMoveMatrix.print_move_matrix()
 
 
 
-myRope = RopeMove()
-myRope.Head = myRope
-myRope.Tail = myRope
-myRope.Start = myRope
-myRope.move_head_right_single()
-print(myRope.Head, myRope.Tail)
-myRope.print_movement_matrix()
-myRope.move_head_right_multiple(4)
-print(myRope.Head, myRope.Tail)
-myRope.print_movement_matrix()
+
+
+
+
 
 
 
